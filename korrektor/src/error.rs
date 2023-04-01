abd04c3b0918156ac43729fa1f337ac9737bdc46
Crystal Kate <crystalnyan@icloud.com>
2023-04-01 16:14:44 +0500

    (error) impl Display for KorrektorError

--- korrektor/src/error.rs
+++ korrektor/src/error.rs
@@ -1,4 +1,15 @@
+use std::fmt::{Display, Formatter};
+
 #[derive(Debug)]
 pub enum KorrektorError {
     InvalidChar(char)
+}
+
+impl Display for KorrektorError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        match self {
+            KorrektorError::InvalidChar(char) =>
+                write!(f, "Invalid character: \"{char}\"! Only Latin and Cyrillic alphabets for Uzbek language are supported.")
+        }
+    }
 }
