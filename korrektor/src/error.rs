9524bd045edcb0ae43061261933cfbce4487e737
Crystal Kate <crystalnyan@icloud.com>
2023-04-01 17:34:59 +0500

    (error) add invalid and overflow number errors

--- korrektor/src/error.rs
+++ korrektor/src/error.rs
@@ -1,15 +1,21 @@
 use std::fmt::{Display, Formatter};
 
 #[derive(Debug)]
-pub enum KorrektorError {
-    InvalidChar(char)
+pub enum KorrektorError{
+    InvalidChar(char),
+    InvalidNumber(String, String),
+    NumberOverflow(String, String)
 }
 
 impl Display for KorrektorError {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         match self {
             KorrektorError::InvalidChar(char) =>
-                write!(f, "Invalid character: \"{char}\"! Only Latin and Cyrillic alphabets for Uzbek language are supported.")
+                write!(f, "Invalid character: \"{char}\"! Only Latin and Cyrillic alphabets for Uzbek language are supported."),
+            KorrektorError::InvalidNumber(number, message) =>
+                write!(f, "{message}: {number}!"),
+            KorrektorError::NumberOverflow(number, message) =>
+                write!(f, "{message}: {number}")
         }
     }
 }
